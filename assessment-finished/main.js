fd58c4524b7bf73eda69f4daf30e32f4768f752f
kako917 <95006568+kako917@users.noreply.github.com>
2022-01-08 04:47:39 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = 'その日は、とても暑かった。そのため、:insertx:は散歩に出かけた。彼らが:inserty:にたどり着いたとき、彼らは少しの間目を見張り、そして :insertz:。 Bob は全貌を見たが, しかし驚きはしなかった。 — :insertx:は300 poundsの重さをかけられていて、その日はとても暑かった。';
+const storyText = 'その日は、とても暑かった。そのため、:insertx:は散歩に出かけた。彼らが:inserty:にたどり着いたとき、彼らは少しの間目を見張り、そして :insertz:。 Bob は全貌を見たが, 驚きはしなかった。 — 体重150kgの:insertx:は驚かなかった。:inserty:ではよくあることだった。';
 const insertX = ['ゴブリン', 'ビッグ・ダディ', 'ファーザー・クリスマス'];
 const insertY = ['スープキッチン', 'ディズニーランド', 'ホワイトハウス'];
 const insertZ = ['自然に発火した', '歩道の水溜りに溶けていった', 'なめくじに変身して這っていった'];
@@ -35,7 +35,7 @@ function result() {
     const weight = `${Math.round(300*0.0714286)} stone`;
     const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
     newStory = newStory.replace('94 fahrenheit', temperature);
-    newStory = newStory.replace('300 pounds', weight);
+    newStory = newStory.replace('150kg', weight);
   }
 
   story.textContent = newStory;
